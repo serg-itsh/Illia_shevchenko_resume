1700092b00e355061b5133dfc09ccb1e3444b68e
Sergey Shevchenko <98112335+serg-itsh@users.noreply.github.com>
2022-11-22 23:12:08 +0200

    New collection.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,16 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-
-import './App.css';
-
-import Header from './Header/Header';
-import Home from './Home/Home';
-
-import Portfolio from './Portfolio/Portfolio';
-import About from './About/About';
-import Footer from './Footer/Footer';
-import NotFound from './NotFound/NotFound';
-
-export const App = props => {
+export const App = () => {
   return (
-    <div className="app_wrapper">
-      <Header />
-      {/* <Home/> */}
-      
-
-      <Routes>
-      <Route path="/" element={<Home />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<NotFound />} />
-
-      </Routes>
-      <Footer/>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
     </div>
   );
 };
